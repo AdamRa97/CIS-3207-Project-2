fe5a4de3a65af025a6f92e7d09d7f89cff940e18
AdamRa97 <adamra97@gmail.com>
2023-03-12 16:49:50 -0400

    Deleted unnessecary .c files since they weren't built-in, added a core shell .c file for built-in commands, added placeholders for said built-in commands, and added more libraries to use

--- shell.c
+++ shell.c
@@ -1,11 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "helpers.h"
 
 int main(void){
+    char input[1024];
+
     while (1){
+        printf("> ");
+        fgets(input, 1024, stdin);
+        char * line = strdup(input);
+        char ** array = parse(line," \n");
 
+        if (array == NULL)
+            exit(1);
 
+        int i = 0;
+        while (array[i] != NULL)
+            printf("s\n",array[i++]);
+        free(array);
+        free(line);
     }
 
+    return 0;
+}
+
+void pwd(void){
+    return 0;
+}
+
+void cd(void){
+    return 0;
+}
+
+void help(void){
     return 0;
 }
