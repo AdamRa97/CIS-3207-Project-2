478e0912a511f534f88bcad5bfb1686d84d7dd54
AdamRa97 <adamra97@gmail.com>
2023-03-14 13:15:30 -0400

    Finished PWD and Exit built in commands

--- shell.c
+++ shell.c
@@ -11,23 +11,24 @@ int help(void);
 int (*find_command(char *name))(void);
 
 // A struct designed for commands, has name property and a pointer to the func
-typedef struct{
-    const char *name;
-    int (*func)(void);
-} command;
+// typedef struct{
+//     const char *name;
+//     int (*func)(void);
+// } command;
 
 // Array of built-in commands
-command commands[] = {
-    {"pwd", pwd},
-    {"cd", cd},
-    {"help", help}
-};
+// command commands[] = {
+//     {"pwd", pwd},
+//     {"cd", cd},
+//     {"help", help}
+// };
 
 int main(){
     char input[1024];
     bool status = true;
 
     while (status){
+
         printf("> ");
         fgets(input, 1024, stdin);
         char * line = strdup(input);
@@ -38,15 +39,16 @@ int main(){
 
         int i = 0;
         while (array[i] != NULL){
-            printf("%s\n",array[i]);
-            int (*command_func)(void) = find_command(array[i]);
+            // printf("%s\n",array[i]);
+            int (*command_func)(void) = find_command(array[i++]);
 
-            if (command_func != NULL)
+            if (command_func != NULL){
                 status = command_func();
+            }
             // else
             //     printf("Command not found: %s\n", array[i]);
 
-            i++;
+            // i++;
         }
 
         free(line);
@@ -56,7 +58,13 @@ int main(){
 }
 
 int pwd(void){
-    printf("hello\n");
+    char cwd[1024];
+    if (getcwd(cwd,sizeof(cwd)) != NULL)
+        printf("%s\n",cwd);
+    else{
+        perror("getcwd() error");
+        return 1;
+    }
     return 0;
 }
 
@@ -70,13 +78,16 @@ int help(void){
     return 0;
 }
 
-
 // Find Command function that searches through the struct command array, and returns the pointer to it if found
 int (*find_command(char *name))(void) {
-    for (int i = 0; commands[i].name != NULL; i++) {
-        if (strcmp(name, commands[i].name) == 0) {
-            return commands[i].func;
-        }
-    }
-    return NULL;
+    if (strcmp(name, "pwd") == 0)
+        return &pwd;
+    else if (strcmp(name, "cd") == 0)
+        return &cd;
+    else if (strcmp(name, "help") == 0)
+        return &help;
+    else if (strcmp(name, "exit") == 0)
+        exit(0);
+    else
+        return NULL;
 }
